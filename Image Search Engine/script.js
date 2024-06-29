d5523cf5fc4e33b8ac9558eb3ddcfd0260ea9690
Unaiz Siddiqui <unaiz4321@gmail.com>
2024-06-30 02:00:51 +0530

    Add Calculator app

--- Image Search Engine/script.js
+++ Image Search Engine/script.js
@@ -3,17 +3,21 @@ const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
 let keyword = "";
 let page = 1;
+
 async function searchImages() {
   keyword = searchBox.value;
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
   const response = await fetch(url);
   const data = await response.json();
   const results = data.results;
+
   if (page === 1) {
     searchResult.innerHTML = "";
   }
+
   results.map((result) => {
     const image = document.createElement("img");
     image.src = result.urls.small;
